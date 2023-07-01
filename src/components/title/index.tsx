0de21d73ca92f75e0bd8560af65f821160b1f140
Agustin Kassis <webmaster@masize.com>
2023-07-01 02:59:49 -0300

    changed titles

--- src/components/title/index.tsx
+++ src/components/title/index.tsx
@@ -1,7 +1,7 @@
 export const Title = () => {
   return (
     <h1 className="text-center text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
-      <div className="text-[hsl(280,100%,70%)]">NOSTR Hackaton</div>
+      <div className="text-[hsl(280,100%,70%)]">Votación</div>
       <div className="text-4xl">
         <span className="text-2xl">by</span> La Crypta
       </div>
